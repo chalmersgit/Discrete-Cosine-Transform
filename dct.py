228ea4223ace2fa2364e17e8b9feecf8905af6f6
Andrew Chalmers <andrewchalmers26@gmail.com>
2018-11-05 10:39:40 +1300

    Simplified by removing x,y normalisation. Added input arguments. New method to call for the basis image.

--- dct.py
+++ dct.py
@@ -15,60 +15,59 @@ I've left this unoptimised for clarity.
 Resources:
 Image and video processing: From Mars to Hollywood with a stop at the hospital,
 lecture 10. By Guillermo Sapiro from Duke University
+https://www.youtube.com/watch?v=pZuaOjfsv0Y&list=PLZ9qNFMHZ-A79y1StvUUqgyL-O0fZh2rs&index=10
+https://www.youtube.com/watch?v=_bltj_7Ne2c&list=PLZ9qNFMHZ-A79y1StvUUqgyL-O0fZh2rs&index=11
 '''
 
+import sys
 from math import cos,sqrt,pi
 import numpy as n_
 import pylab as p_
 import matplotlib.cm as cm
 
-def dct(x, y, v, u, n):
+def dct(x, y, u, v, n):
     # Normalisation
     def alpha(a):
         if a==0:
             return sqrt(1.0/n)
         else:
             return sqrt(2.0/n)
-    
-    return alpha(u)*alpha(v)*cos(((2*x+1)*(u*pi))/(2*n))*cos(((2*y+1)*(v*pi))/(2*n))
-    
-if __name__ == '__main__':
-    print "Running DCT"
-    n = 8 # Assume square image, so we don't have different xres and yres
-    
-    # We can get different frequencies by setting u and v
-    # Here, we have a max u and v to loop over and display
-    # Feel free to adjust
-    maxV = n
-    maxU = n
-    
-    imageSet = []
-    for u in range(0, maxU):
-        for v in range(0, maxV):
-            basisImg = n_.zeros((n,n))
-            for y in range(0, n):
-                for x in range(0, n):    
-                    basisImg[y,x] = dct(x, y, v, u, max(n, maxV))
-            imageSet.append(basisImg)
-    
-    '''
-    # Instead of looping, we can choose one basis(u,v)
-    v = 0
-    u = 0
+    return alpha(u) * alpha(v) * cos(((2*x+1)*(u*pi))/(2*n)) * cos(((2*y+1)*(v*pi))/(2*n))
+
+def getBasisImage(u, v, n):
+    # for a given (u,v), make a DCT basis image
     basisImg = n_.zeros((n,n))
     for y in range(0, n):
-        for x in range(0, n):    
-            basisImg[y,x] = dct(x, y, v, u, n)
-    '''
-    
-    displayRows = maxV
-    displayCols = maxU
+        for x in range(0, n):
+            basisImg[y,x] = dct(x, y, u, v, n)
+    return basisImg
+
+if __name__ == '__main__':
+    print("Running DCT")
+
+    # n (int): assume square image, so we don't have different xres and yres
+    try:
+        n = int(sys.argv[1])
+    except:
+        n = 8
+    print("n: %d" % n)
+
+    # We can get different frequencies by setting u and v
+    print("Generating DCT basis images")
+    imageSet = []
+    for u in range(0, n):
+        for v in range(0, n):
+            basisImg = getBasisImage(u, v, n)
+            imageSet.append(basisImg)
+
+    print("Saving figure...")
     p_.figure("DCT")
     for i in range(0, len(imageSet)):
-        p_.subplot(displayRows, displayCols, i+1)
+        p_.subplot(n, n, i+1)
         p_.axis('off')
         p_.imshow(imageSet[i],cmap = cm.Greys_r)
-    p_.savefig('_dct_'+str(n)+'x'+str(n)+'_'+str(maxU)+'x'+str(maxV)+'.png')
+    p_.savefig('_dct_'+str(n)+'x'+str(n)+'_'+str(n)+'x'+str(n)+'.png')
     p_.show()
-    print "Complete" 
+
+    print("Complete" )
     
